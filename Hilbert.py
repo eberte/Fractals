f0abc10113cf0c6f01e698ecf36519a50449e81f
CS Student <csstudent@CompSci9.local>
2017-11-14 10:51:37 -0600

    updates

--- Hilbert.py
+++ Hilbert.py
@@ -10,26 +10,40 @@ def hilbert(tortoise, reverse, depth):
     if depth < 0:
         tortoise.fd(0)
 
+    if reverse == True:
+        tortoise.rt(angle)
+        hilbert(tortoise, reverse, depth - 1)
+        tortoise.fd(10)
+        tortoise.lt(angle)
+        hilbert(tortoise, reverse, depth - 1)
+        tortoise.fd(10)
+        hilbert(tortoise, reverse, depth - 1)
+        tortoise.lt(angle)
+        tortoise.fd(10)
+        hilbert(tortoise, reverse, depth - 1)
+        tortoise.rt(angle)
+        
     else:
-        tortoise.rt(angle)
-        hilbert(tortoise, reverse, depth - 1)
-        tortoise.fd(10)
         tortoise.lt(angle)
         hilbert(tortoise, reverse, depth - 1)
         tortoise.fd(10)
+        tortoise.rt(angle)
         hilbert(tortoise, reverse, depth - 1)
-        tortoise.lt(angle)
         tortoise.fd(10)
         hilbert(tortoise, reverse, depth - 1)
         tortoise.rt(angle)
+        tortoise.fd(10)
+        hilbert(tortoise, reverse, depth - 1)
+        tortoise.lt(angle)
+        
     
-  
+
 
 def main():
-    reverse = False
+    reverse = True
     myTurtle = turtle.Turtle()
     myTurtle.lt(90)
-    hilbert(myTurtle, reverse, 1)
+    hilbert(myTurtle, reverse, 0)
     screen = myTurtle.getscreen()
     screen.exitonclick()
 
