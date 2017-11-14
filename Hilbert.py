0d7181987cb10813d69912630c34340c45a47313
CS Student <csstudent@CompSci9.local>
2017-11-14 11:41:55 -0600

    Update II

--- Hilbert.py
+++ Hilbert.py
@@ -1,40 +1,35 @@
 import turtle
 
 def hilbert(tortoise, reverse, depth):
-    if reverse == True:
-        angle = 270
-        
-    else:
-        angle = 90
         
     if depth < 0:
-        tortoise.fd(0)
+        return
 
     if reverse == True:
-        tortoise.rt(angle)
+        tortoise.rt(90)
+        hilbert(tortoise, False, depth - 1)
+        tortoise.fd(10)
+        tortoise.lt(90)
         hilbert(tortoise, reverse, depth - 1)
         tortoise.fd(10)
-        tortoise.lt(angle)
         hilbert(tortoise, reverse, depth - 1)
+        tortoise.lt(90)
         tortoise.fd(10)
-        hilbert(tortoise, reverse, depth - 1)
-        tortoise.lt(angle)
-        tortoise.fd(10)
-        hilbert(tortoise, reverse, depth - 1)
-        tortoise.rt(angle)
+        hilbert(tortoise, False, depth - 1)
+        tortoise.rt(90)
         
     else:
-        tortoise.lt(angle)
+        tortoise.lt(90)
+        hilbert(tortoise, True, depth - 1)
+        tortoise.fd(10)
+        tortoise.rt(90)
         hilbert(tortoise, reverse, depth - 1)
         tortoise.fd(10)
-        tortoise.rt(angle)
         hilbert(tortoise, reverse, depth - 1)
+        tortoise.rt(90)
         tortoise.fd(10)
-        hilbert(tortoise, reverse, depth - 1)
-        tortoise.rt(angle)
-        tortoise.fd(10)
-        hilbert(tortoise, reverse, depth - 1)
-        tortoise.lt(angle)
+        hilbert(tortoise, True, depth - 1)
+        tortoise.lt(90)
         
     
 
@@ -43,7 +38,7 @@ def main():
     reverse = True
     myTurtle = turtle.Turtle()
     myTurtle.lt(90)
-    hilbert(myTurtle, reverse, 0)
+    hilbert(myTurtle, reverse, 5)
     screen = myTurtle.getscreen()
     screen.exitonclick()
 
